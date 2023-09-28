be52ee84ee570b791d224f3907ffd385817bf1bf
Chris Birchall <chris.birchall@gmail.com>
2023-09-28 20:53:49 +0100

    Rename again

--- dist/dusty-study-7811c3ccea58be90.js
+++ dist/dusty-study-7811c3ccea58be90.js
@@ -825,7 +825,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('gram-f5f4fd4075c99ab3_bg.wasm', import.meta.url);
+        input = new URL('dusty-study-7811c3ccea58be90_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
