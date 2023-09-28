604febf951a3ee06a20192f1a0e9963ab15acfe0
Chris Birchall <chris.birchall@gmail.com>
2023-09-28 22:01:47 +0100

    Finish implementing codegen
    But it generates a .rs file so huge it crashes the compiler.
    
    Next step is to try `include_str!` + JSON parsing

--- dataset/src/main.rs
+++ dataset/src/main.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{Write, Error};
 use itertools::Itertools;
+use codegen::Scope;
 
 fn read_words(filename: &str) -> Vec<String> {
     read_to_string(filename)
@@ -30,12 +33,40 @@ fn build_map(words: Vec<String>) -> HashMap<String, Vec<String>> {
 
     // remove any entries with singleton list values
     // because it means those words have no anagrams
-    hashmap.retain(|_, v| v.len() > 1);
+    //
+    // -- stopped doing this, because we often the words
+    // in the crossword clue are not a real phrase in our dataset,
+    // e.g. "so I sync diary" -> idiosyncrasy
+    //hashmap.retain(|_, v| v.len() > 1);
 
     hashmap
 }
 
-fn main() {
+fn generate_code(hashmap: HashMap<String, Vec<String>>) -> Scope {
+    let mut scope = Scope::new();
+
+    scope.import("std::collections", "HashMap");
+
+    let function = scope
+        .new_fn("build_map")
+        .allow("dead_code")
+        .ret("HashMap<& 'static str, Vec<& 'static str>>");
+
+    function.line("HashMap::from([");
+    for (k, vs) in hashmap {
+        let values = vs
+            .into_iter()
+            .map(|x| format!("\"{}\"", x))
+            .collect::<Vec<String>>()
+            .join(", ");
+        function.line(format!("(\"{}\", vec![{}])", k, values));
+    }
+    function.line("])");
+
+    scope
+}
+
+fn main() -> Result<(), Error> {
     let mut words = read_words("./words-uniq.txt");
     println!("{} lines read from input file", words.len());
 
@@ -48,5 +79,9 @@ fn main() {
     let hashmap = build_map(words);
     println!("{} anagram keys", hashmap.len());
 
-    // TODO codegen
+    let scope = generate_code(hashmap);
+    println!("Generated code");
+
+    let mut output = File::create("../netlify/functions/hello/src/database/data/generated.rs")?;
+    write!(output, "{}", scope.to_string())
 }
